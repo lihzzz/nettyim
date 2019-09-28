7a0a8f632d3ad000ddb1b274e3f3f1e104e206ec
lihzz <lihzz0323@gmail.com>
2019-09-28 23:25:07 +0800

    9.28

--- src/main/java/test/ByteBufTest.java
+++ src/main/java/test/ByteBufTest.java
@@ -3,8 +3,18 @@ package test;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 
+import static jdk.nashorn.internal.objects.Global.print;
+
 public class ByteBufTest {
     public static void main(String[] args) {
         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9,100);
+        print("allocate ByteBuf(9, 100)", buffer);
+        buffer.writeBytes(new byte[]{1,2,3,4});
+        print("writeBytes(1,2,3,4)", buffer);
+
+        buffer.writeInt(12);
+        print("writeInt(12)", buffer);
+
+
     }
 }
